e360efcb35f22cc998997cce902bbf0574ef607b
Yagnik-Gohil <gohilyagnik3@gmail.com>
2024-10-09 21:37:46 +0530

    feat: change background on each route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom"; // Import useLocation
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import NavBar from "./components/NavBar";
@@ -9,6 +9,21 @@ import { useState } from "react";
 function App() {
   const [path, setPath] = useState(MENU);
   const [display, setDisplay] = useState<"none" | "flex">("none");
+
+  // Get the current location
+  const location = useLocation();
+
+  // Map routes to background classes
+  const bgClasses: { [key: string]: string }  = {
+    "/": "bg-home-gradient",
+    "/about": "bg-about-gradient",
+    "/portfolio": "bg-portfolio-gradient",
+    "/services": "bg-services-gradient",
+    "/contact": "bg-contact-gradient",
+  };
+
+  // Get the corresponding background class for the current route
+  const bgClass = bgClasses[location.pathname] || "bg-home-gradient"; // Fallback to home-gradient
 
   const handleMenu = () => {
     if (path === MENU) {
@@ -21,7 +36,7 @@ function App() {
   };
 
   return (
-    <div className="bg-home-gradient h-dvh flex justify-center items-center px-6 py-5 md:p-12">
+    <div className={`${bgClass} h-dvh flex justify-center items-center px-6 py-5 md:p-12`}>
       <div className="bg-[#000000d9] w-[100%] h-[100%] rounded-2xl max-w-[1440px] max-h-[1080px] flex flex-col">
         <NavBar />
         <div className="flex overflow-hidden flex-1 relative rounded-b-2xl">
@@ -34,7 +49,7 @@ function App() {
               <img
                 src={path}
                 className="w-[20px] h-[20px] opacity-65 select-none pointer-events-none max-w-[100%] inline-block align-middle z-[2]"
-              ></img>
+              />
             </div>
             <Routes>
               <Route path="/" element={<Home />} />
